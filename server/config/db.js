46cabf7f53fd7a98029f189e3ca05803502948a3
okan sezgin <sezginokan.62@gmail.com>
2020-10-30 17:37:51 -0400

    update db

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
-};
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://OkanSezgin:Dersim6292@mongodbserver.pomzb.mongodb.net/books229?retryWrites=true&w=majority"
+}
